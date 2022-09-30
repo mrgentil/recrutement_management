30e6403dfc83679ba0a274c475f52daf0e17e1c6
mrgentil <tshitshob@gmail.com>
2022-09-30 11:52:12 +0100

    fixed_connection

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -42,8 +42,7 @@ class User extends Authenticatable implements JWTSubject
      *
      * @return mixed
      */
-    public function getJWTIdentifier(): mixed
-    {
+    public function getJWTIdentifier() {
         return $this->getKey();
     }
     /**
@@ -51,8 +50,7 @@ class User extends Authenticatable implements JWTSubject
      *
      * @return array
      */
-    public function getJWTCustomClaims(): array
-    {
+    public function getJWTCustomClaims() {
         return [];
     }
 }
